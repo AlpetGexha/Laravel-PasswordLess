d545d6a1d8f1c81bbfccb2c2fbb934ac8189f832
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-04-21 00:28:02 +0200

    Update LoginLink.php

--- app/Mail/Auth/LoginLink.php
+++ app/Mail/Auth/LoginLink.php
@@ -9,14 +9,13 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\URL;
 
 final class LoginLink extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public function __construct(public readonly string $url)
-    {
-    }
+    public function __construct(public readonly string|URL $url){}
 
     public function envelope(): Envelope
     {
